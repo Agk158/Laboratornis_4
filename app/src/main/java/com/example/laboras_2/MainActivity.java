4d9be8106af9621c8773471ee138421a9062d331
Agk158 <agne.kazlauskaite@knf.stud.vu.lt>
2021-05-03 23:13:16 +0300

    selector

--- app/src/main/java/com/example/laboras_2/MainActivity.java
+++ app/src/main/java/com/example/laboras_2/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.laboras_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    Spinner Selector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.Selector = findViewById(R.id.Selector);
+
+
+        ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(this, R.array.options, android.R.layout.simple_spinner_item);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        this.Selector.setAdapter(arrayAdapter);
     }
+
 }
