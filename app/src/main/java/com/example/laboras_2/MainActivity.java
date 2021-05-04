d72b1a4308ec9d7f3a1cd3b776641a0f0e736671
Agk158 <agne.kazlauskaite@knf.stud.vu.lt>
2021-05-04 19:41:23 +0300

    code added

--- app/src/main/java/com/example/laboras_2/MainActivity.java
+++ app/src/main/java/com/example/laboras_2/MainActivity.java
@@ -3,10 +3,13 @@ package com.example.laboras_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.GetChars;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -27,4 +30,20 @@ public class MainActivity extends AppCompatActivity {
         this.Selector.setAdapter(arrayAdapter);
     }
 
+    public void OnBtnClick (View view) {
+        EditText InputText = findViewById( R.id.InputText );
+        TextView Output = findViewById( R.id.Output );
+        String userInputText = InputText.getText().toString();
+
+        String selection = this.Selector.getSelectedItem().toString();
+        String resValue = getResources().getString( R.string.char_selection );
+        Toast.makeText(this, String.valueOf( resValue), Toast.LENGTH_SHORT).show();
+
+
+        if (selection.equalsIgnoreCase(resValue)) {
+            int count = userInputText.length();
+            Output.setText( String.valueOf(count));
+        }
+
+    }
 }
